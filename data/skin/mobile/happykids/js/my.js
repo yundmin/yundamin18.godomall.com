879308a9feda8ed0d5c50eccd6e4b0f0a05e618f
윤다민 <yundamin@naver.com>
2023-02-05 16:04:20 +0900

    dd

--- data/skin/mobile/happykids/js/my.js
+++ data/skin/mobile/happykids/js/my.js
@@ -1,9 +1,9 @@
 
 $(document).ready(function () {
 
-    $(document).on('click', '.quick_box a:nth-of-type(1)', function(){
-        window.location.href='http://m.yundamin18.godomall.com/goods/goods_list.php?cateCd=001'
-    })
+    $(document).on("click", ".quick_box a:nth-of-type(1)", function(){
+        window.location.href="http://m.yundamin18.godomall.com/goods/goods_list.php?cateCd=001";
+    });
 
     $("#goods_fp").fullpage({
 
