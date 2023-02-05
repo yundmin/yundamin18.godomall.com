3de05cc0e3235adde9f19df37d263760d56f6825
윤다민 <yundamin@naver.com>
2023-02-05 14:48:45 +0900

    dd

--- data/skin/mobile/happykids/js/my.js
+++ data/skin/mobile/happykids/js/my.js
@@ -18,22 +18,11 @@
         verticalCentered: true,
         // fixedElements : '#header, .footer',
         fitToSection: true,
-        afterResponsive: function (isResponsive) {},
-        responsiveHeight: 600,
-        afterResponsive: function (isResponsive) {},
-        afterLoad: function (origin, destination, direction) {
-            $(destination.item).find("wow").addClass("animated fadeInUp");
-        },
+
         navigationPosition: "right",
-        navigationTooltips: ["섹션1", "섹션2", "섹션3", "섹션4"],
+
         css3: true,
-        onLeave: function (index, nextIndex, direction) {
-            //lazyload background
-            if (nextIndex === 3) {
-                var destinationSection = $(".section4").eq(nextIndex - 1);
-                destinationSection.addClass("맨마지막백그라운드");
-            }
-        },
+
     });
     $("로고").click(function () {
     $.fn.fullpage.moveTo(1);
