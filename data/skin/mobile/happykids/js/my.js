ae9e7a41c997e5df8d2ce719b7869bb83ce86f9d
윤다민 <yundamin@naver.com>
2023-02-05 15:17:32 +0900

    dd

--- data/skin/mobile/happykids/js/my.js
+++ data/skin/mobile/happykids/js/my.js
@@ -3,17 +3,17 @@ $(document).ready(function () {
     $("#goods_fp").fullpage({
 
         scrollingSpeed: 1000,
-        slidesNavigation: true,
-        navigation: true,
+
+        // navigation: true,
         sectionSelector: ".goods_fp_list",
         slideSelector: ".slide",
-        slideNavigation: true,
-        keyboardScrolling: true,
+        // slideNavigation: true,
+        // keyboardScrolling: true,
         paddingTop: "",
         touchSensitivity: 15,
-
+        // animateAnchor: true,
         scrollOverflow: true,
-
+        // controlArrows: true,
 
         verticalCentered: true,
 
